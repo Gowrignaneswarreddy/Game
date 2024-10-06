e79977bdb7f8e73756f08053999f43f4717fa9f9
Gowrignaneswarreddy <gowrignaneswarreddy@gmail.com>
2024-10-06 14:00:26 +0530

    second project 

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Game from "./components/Game";
+let App =()=>{
+    return(
+        <>
+        <div>
+            <Game/>
+            </div>    
+        </>
+    )
+}
+
+export default App
